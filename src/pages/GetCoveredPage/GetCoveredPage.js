d3fd586c98d8ca87f4b1771c09bfeaaf9f2f4da0
rishabhkeshan <rishabhkeshan@gmail.com>
2023-07-01 20:29:57 +0530

    added dummy icons

--- src/pages/GetCoveredPage/GetCoveredPage.js
+++ src/pages/GetCoveredPage/GetCoveredPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import logo from "../../logo.svg";
 import "./GetCoveredPage.scss";
-import PeerShield from "../../assets/PeerShield_Dark.svg";
+import Osmosis from "../../assets/Osmosis.svg";
 import MyCover from "../../assets/MyCoverIcon.svg";
 import { Link, useLocation } from "react-router-dom";
 import { mainnetChains, useAccount, useConnect, useDisconnect } from "graz";
@@ -47,6 +47,7 @@ function GetCovered() {
       premiumPricing: "$10,000",
       coverProvided: "Up to $1,000,000",
       insuranceCapacity: "$50,000,000",
+      icon: Osmosis,
     },
     {
       protocolName: "AssetMantle",
@@ -54,6 +55,7 @@ function GetCovered() {
       premiumPricing: "$8,000",
       coverProvided: "Up to $500,000",
       insuranceCapacity: "$20,000,000",
+      icon: Osmosis,
     },
     {
       protocolName: "Persistence",
@@ -61,6 +63,7 @@ function GetCovered() {
       premiumPricing: "$5,000",
       coverProvided: "Up to $250,000",
       insuranceCapacity: "$10,000,000",
+      icon: Osmosis,
     },
   ];
   return (
@@ -90,36 +93,42 @@ function GetCovered() {
           />
         </div>
         {activeTabIndex === 0 && (
-            <div className="grid-container">
-              {protocolsData.map((protocol, index) => (
-                <div key={index} className="card">
+          <div className="getcovered_coverssection_allcovers">
+            {protocolsData.map((protocol, index) => (
+              <div
+                key={index}
+                className="getcovered_coverssection_allcovers_card"
+              >
+                <div className="flex items-start">
+                  <img className="w-12 mr-3 " src={protocol.icon} alt="protocol icon"/>
                   <div>
-                    <h3 className="protocol-name">{protocol.protocolName}</h3>
-                    <p className="vulnerability">{protocol.vulnerability}</p>
-                  </div>
-                  <div>
-                    <div className="details">
-                      <span className="details-label">Premium Pricing:</span>
-                      <span className="details-value">
-                        {protocol.premiumPricing}
-                      </span>
-                    </div>
-                    <div className="details">
-                      <span className="details-label">Cover Provided:</span>
-                      <span className="details-value">
-                        {protocol.coverProvided}
-                      </span>
-                    </div>
-                    <div className="details">
-                      <span className="details-label">Insurance Capacity:</span>
-                      <span className="details-value">
-                        {protocol.insuranceCapacity}
-                      </span>
-                    </div>
+                  <h3 className="protocol-name">{protocol.protocolName}</h3>
+                  <p className="vulnerability">{protocol.vulnerability}</p>
                   </div>
                 </div>
-              ))}
-            </div>
+                <div>
+                  <div className="details">
+                    <span className="details-label">Premium Pricing:</span>
+                    <span className="details-value">
+                      {protocol.premiumPricing}
+                    </span>
+                  </div>
+                  <div className="details">
+                    <span className="details-label">Cover Provided:</span>
+                    <span className="details-value">
+                      {protocol.coverProvided}
+                    </span>
+                  </div>
+                  <div className="details">
+                    <span className="details-label">Insurance Capacity:</span>
+                    <span className="details-value">
+                      {protocol.insuranceCapacity}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         )}
       </section>
     </article>
